fa994404c430898613618c4b0b43d0a40eb8a6d5
Peter Cai <peter@typeblog.net>
2016-03-11 15:00:53 +0000

    hello.sh: new command: /echo
    Signed-off-by: Peter Cai <peter@typeblog.net>

--- mods/hello.sh
+++ mods/hello.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 command "hello" cmd_hello "Just hello"
+command "echo" cmd_echo "Echo the arguments passed to this command."
 
 cmd_hello() {
 	parse_msg
 	api_sendMessage \
 		-d "chat_id=$msg_chat_id" \
 		-d "text=Hello, @$msg_from_username" \
+		> /dev/null
+}
+
+cmd_echo() {
+	parse_msg
+	api_sendMessage \
+		-d "chat_id=$msg_chat_id" \
+		-d "text=$@" \
 		> /dev/null
 }
